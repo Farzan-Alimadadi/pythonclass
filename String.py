6df197931f06c5a97a829b5911555367ff1077df
Farzan-Alimadadi <farzanalimadadi.1999@gmail.com>
2023-01-01 20:34:34 +0330

    String exercise

---
+++ String.py
@@ -0,0 +1,10 @@
+a = "a b c d\t"
+b = "e f g h\t"
+c = "i j k l\t"
+d = "m n o p\t"
+e = "q r s t\t"
+f = "u v w x\t"
+g = "y z"
+letters = "{} {} {} {} {} {} {}" .format(a, b, c, d, e, f, g)
+print(letters)
+print (a, b, c, d, e, f, g)
